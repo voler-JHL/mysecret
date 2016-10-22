ecfc596e373ad56a420e4c0f682a7fde985e2e14
HJL_voler <13039201005@163.com>
2016-10-22 17:19:38 +0800

    添加注册界面

--- myapp/src/main/java/com/voler/myapplication/mvp/ui/activity/RegisterActivity.java
+++ myapp/src/main/java/com/voler/myapplication/mvp/ui/activity/RegisterActivity.java
@@ -1,10 +1,22 @@
 package com.voler.myapplication.mvp.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.jess.arms.utils.DataHelper;
+import com.jess.arms.utils.UiUtils;
 import com.voler.myapplication.R;
+import com.voler.myapplication.util.StringUtil;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static com.voler.myapplication.util.StringUtil.sha2;
 
 /**
  * 妈妈生活圈
@@ -12,10 +24,34 @@ import com.voler.myapplication.R;
  * Created by voler on 2016/10/20.
  */
 
-public class RegisterActivity extends AppCompatActivity{
+public class RegisterActivity extends AppCompatActivity {
+    @Bind(R.id.et_input_password1)
+    EditText etInputPassword1;
+    @Bind(R.id.et_input_password2)
+    EditText etInputPassword2;
+    @Bind(R.id.tv_register)
+    TextView tvRegister;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.tv_register)
+    public void onClick() {
+        String pd1 = etInputPassword1.getText().toString().trim();
+        String pd2 = etInputPassword2.getText().toString().trim();
+        if (pd1==null||"".equals(pd1)){
+            UiUtils.makeText("请输入密码");
+        }else if (!pd1.equals(pd2)){
+            UiUtils.makeText("两次输入的密码不一致");
+        }else {
+            String myPassword = StringUtil.sha2(StringUtil.sha2(pd1) + sha2(pd1 + StringUtil.salt));
+            DataHelper.SetStringSF(this,"password",myPassword);
+            Intent intent=new Intent(this,RealMainActivity.class);
+            startActivity(intent);
+        }
     }
 }
